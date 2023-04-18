cd52f410e45222ade3b339a640f661995f070860
junhkim20 <jk7048@nyu.edu>
2023-04-17 23:03:32 -0400

    Basics and JSON Data

---
+++ script.js
@@ -0,0 +1,26 @@
+let evCarCompanyCollection = [
+	{
+		"companyName": "Tesla",
+		"startYear": "2003",
+		"headquarter": "Austin, TX",
+		"logo": "Tesla.png"
+	},
+	{
+		"companyName": "Rivian",
+		"startYear": "2009",
+		"headquarter": "Irvine, CA",
+		"logo": "Rivian.png"
+	},
+	{
+		"companyName": "Polestar",
+		"startYear": "1996",
+		"headquarter": "Gothenburg, Sweden",
+		"logo": "Polestar.png"
+	},
+	{
+		"companyName": "Lucid Motors",
+		"startYear": "2007",
+		"headquarter": "Newark, CA",
+		"logo": "Lucid.png"
+	}
+];
